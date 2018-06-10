ace7db1ec64611786fe1b98cf72f0bad85bb2093
Dmitry Kisler <admin@dkisler.de>
2018-06-10 22:49:26 +0200

    Update datUtils.R

--- functions/datUtils.R
+++ functions/datUtils.R
@@ -7,7 +7,7 @@
 datFetcher <- function() {
   library(jsonlite, verbose = F, quietly = T)
   
-  dat <- tryCatch(fromJSON( gsub("\\@ts\\@", paste0(as.integer(Sys.time()), '000'), "https://shipahoy.io/api/v1/live?start_ts=@ts@&port_id=2&end_ts=@ts@") )$vessels ,
+  dat <- tryCatch(fromJSON( gsub("\\@ts\\@", paste0(as.integer(Sys.time()), '000'), "https://shipahoy.io/#####") )$vessels ,
                   error = function(c) return(data.frame())
   ) %>% data.frame()
   return(dat)
